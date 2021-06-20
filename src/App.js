7615ebcfbbe80808ab52987e332373e72e0d5bc7
Giancarlo <giancarlo@pop-os.localdomain>
2021-06-20 14:58:40 -0400

    Header components and Home page component started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import NavBar from './Components/NavBar/NavBar';
+import NavBar from './Components/navBar/NavBar';
+import Home from './Components/pages/home/Home.jsx';
 
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Home />
     </div>
   );
 };
---
+++ src/Components/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../../header/Header";
+import './home.css';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Header />
+            HomePage
+        </div>
+    );
+}
+
+export default Home;
