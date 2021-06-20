448e093a31ab4d02292d6782067f11dc63df8c05
Giancarlo <giancarlo@pop-os.localdomain>
2021-06-19 23:25:13 -0400

    navbar items

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,9 +2,24 @@ import './navbar.css';
 
 const NavBar = () => {
     return (
-        <div className = "navbar">
-            navbar
-        </div>
+        <>
+            <div className="navbar">
+                <div className="navLeft">
+                    <i class="fas fa-phone"></i>
+                    <i class="fas fa-envelope"></i>
+                </div>
+                <div className="navCenter">
+                    <ul className="navList">
+                        <li className="navListItem">HOME</li>
+                        <li className="navListItem">ABOUT</li>
+                        <li className="navListItem">CONTACT</li>
+                        <li className="navListItem">WRITE</li>
+                        <li className="navListItem">LOGOUT</li>
+                    </ul>
+                </div>
+                <div className="navRight"></div>
+            </div>
+        </>
     );
 }
 
