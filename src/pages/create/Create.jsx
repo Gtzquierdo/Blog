d20349c72bc00307e0092c225442566507c93c44
Giancarlo <giancarlo@pop-os.localdomain>
2021-06-28 23:06:03 -0400

    blog, singlePost and create component complete

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -3,7 +3,20 @@ import './create.css';
 const Create = () => {
     return (
         <div className="create">
-            sss
+        <img className="createImg" src="https://images.pexels.com/photos/261579/pexels-photo-261579.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="postpic"/>
+            <form className="createForm">
+                <div className="createFormGroup">
+                    <label htmlFor="fileInput">
+                        <i class="fas fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display:"none"}} />
+                    <input type="text" placeholder="Title" className="createInput" autoFocus={true} />
+                </div>
+                <div className="createFormGroup">
+                    <textarea placeholder="Tell your story..." type="text" className="createInput createText"></textarea>
+                </div>
+                <button className="createSubmit">Publish</button>
+            </form>
         </div>
     );
 }
