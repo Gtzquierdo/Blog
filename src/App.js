4481575e6d18dfc0da5bba204824d23dd63b52e0
Giancarlo <giancarlo@pop-os.localdomain>
2021-06-29 20:27:51 -0400

    settings page complete with css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import NavBar from './Components/navBar/NavBar';
 import Home from './pages/home/Home.jsx';
 import Single from './pages/single/Single';
 import Create from './pages/create/Create';
+import Setting from './pages/settings/Setting';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <Create />
+      <Setting />
     </div>
   );
 };
--- src/pages/settings/Setting.jsx
+++ src/pages/settings/Setting.jsx
@@ -1,8 +1,33 @@
 import './setting.css';
+import Sidebar from '../../Components/sidebar/Sidebar';
 
 const Setting = () => {
     return (
-        <div>
+        <div className="settings">
+        <div className="settingsWrapper">
+            <div className="settingsTitle">
+                <span className="settingsUpdateTitle">Update Your Account</span>
+                <span className="settingsDeleteTitle">Update Your Account</span>
+                
+            </div>
+            <form className="settingsForm">
+                <label>Profile Picture</label>
+                <div className="settingsProPic">
+                    <img src="https://images.pexels.com/photos/4069292/pexels-photo-4069292.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="pic" />
+                    <label htmlFor="fileInput">
+                        <i className="settingsProPicIcon far fa-user-circle"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display:"none"}}/>
+                </div>
+                <label>Username</label>
+                <input type="text" placeholder="Giancarlo" />
+                <label>Email</label>
+                <input type="email" placeholder="giancarlo@gmail.com" />
+                <label>Password</label>
+                <input type="password"  />
+                <button className="settingsSubmit">Update</button>
+            </form>
+        </div>
             
         </div>
     );
