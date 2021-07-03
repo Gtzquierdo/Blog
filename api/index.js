63ba9fa4e873e11af85d5066e2c636307556a280
Giancarlo <giancarlo@pop-os.localdomain>
2021-07-02 23:15:41 -0400

    auth complete with login and register. moving onto users and posts routes

--- api/index.js
+++ api/index.js
@@ -5,12 +5,13 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth")
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-}).then(console.log("Connected to MongoDB")).catch(err=>console.log(error));
+}).then(console.log("Connected to MongoDB")).catch((err) =>console.log(err));
 
 app.use("/api/auth", authRoute);
 
