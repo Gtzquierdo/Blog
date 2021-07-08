bff834bf6df656b075b9a4f4150461832bd3756d
Giancarlo <giancarlo@pop-os.localdomain>
2021-07-08 17:05:15 -0400

    User Route completed, moving to Post route later

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -30,7 +30,7 @@ router.post("/login", async (req, res) => {
         !validated && res.status(400).json("Wrong credentials!");
 
         const { password, ...others } = user._doc;
-        res.status(200).json(user);
+        res.status(200).json(others);
     } catch (err) {
         res.status(500).json(err);
     }
