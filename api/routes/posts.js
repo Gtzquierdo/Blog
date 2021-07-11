82446f792ca9a2f10728eb0f5ac6a3c581598056
Giancarlo <giancarlo@pop-os.localdomain>
2021-07-10 20:03:01 -0400

    get post and all post complete

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -52,16 +52,36 @@ router.delete("/:id", async (req, res) => {
     }
 });
 
-//Deleting a post
-
 // Get post 
 router.get("/:id", async (req, res) => {
     try{
-        const user = await User.findById(req.params.id);
-        const { password, ...others } = user._doc;
-        res.status(200).json(others);
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
     } catch (err) {
         res.status(500).json(err);
     }
 });
+
+// Get all posts
+router.get("/", async (req, res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
+    try {
+        let posts;
+        if (username) {
+            posts = await Post.find({ username });
+        } else if (catName) {
+            posts = await Post.find({ 
+                categories: 
+                { $in: [catName],
+                },
+            });
+        } else {
+            posts = await Post.find();
+    }   res.status(200).json(posts);
+} catch (err) {
+    res.status(500).json(err);
+}
+});
+
 module.exports = router;
