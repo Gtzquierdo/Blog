95e4c87b7438aeb1d89401999a542c09141d8de8
Giancarlo <giancarlo@pop-os.localdomain>
2021-07-27 22:28:32 -0400

    axios

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,8 +2,17 @@ import Header from "../../Components/header/Header";
 import Sidebar from "../../Components/sidebar/Sidebar";
 import Posts from "../../Components/posts/Posts"
 import './home.css';
+import axios from "axios";
 
 const Home = () => {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async ()=> {
+            axios.get("/")
+        }
+    },[])
+
     return (
         <>
             <Header />
